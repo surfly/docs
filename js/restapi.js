a5bdf8e5f38f6e6f15eb7e96a785daa1f590d71b
izzysmith1 <anonymous@gitbook.com>
2016-07-19 15:55:02 +0000

    Update js/restapi.js

--- js/restapi.js
+++ js/restapi.js
@@ -1,8 +1,9 @@
-
-       document.getElementsByClassName("btn pull-left js-toolbar-action").addEventListener("click", stretchApi);
+    <script type="text/javascript">
+       
+       document.getElementsByClassName("btn pull-left js-toolbar-action").addEventListener("click",        stretchApi() {
+          apiFrame.style.margin="0px0px0px0px";
+       }, false);
        console.log("hideMenu:"+ hideMenu);
        var apiFrame = document.getElementById("restapiFrame");
 
-       function stretchApi() {
-          apiFrame.style.margin="0px0px0px0px";
-       }
+     </script>
